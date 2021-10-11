33583828229bb53155358ae4ca2ba769c8223210
Dylan Floyd <floyd800@gmail.com>
2021-10-11 14:47:13 -0700

    add authenticated todos endpoints and tests

---
+++ data/todos.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    todo: 'thing 1',
+    completed: false,
+    user_id: 1
+  },
+  {
+    todo: 'thing 2',
+    completed: false,
+    user_id: 1
+  },
+  {
+    todo: 'thing 3',
+    completed: false,
+    user_id: 1
+  }
+];
